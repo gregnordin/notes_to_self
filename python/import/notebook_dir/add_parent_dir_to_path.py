c48d5bbb05796a2be17eb6fe27ad0c0aeb12df00
gregnordin <nordin@byu.edu>
2021-04-21 14:26:12 -0600

    Add docstring to insert_dir_into_path

--- python/import/notebook_dir/add_parent_dir_to_path.py
+++ python/import/notebook_dir/add_parent_dir_to_path.py
@@ -3,6 +3,26 @@ import sys
 
 def insert_dir_into_path(cwd, num_levels_up):
     """Starting from cwd, go num_levels_up and insert that directory into PATH.
+    Intended to work with Jupyter notebooks to import packages located up the 
+    directory tree.
+    
+    Arguments
+    ---------
+    cwd: Path object
+        Current working directory, i.e., directory in which Jupyter notebook is located.
+    num_levels_up: int
+        How many directories up from cwd to go
+        
+    Example usage
+    -------------
+    from pathlib import Path
+
+    # Go one directory up and insert it into PATH
+    from add_parent_dir_to_path import insert_dir_into_path
+    cwd = !pwd
+    insert_dir_into_path(Path(cwd[0]), 1)
+
+    import <some_package>
     """
     assert isinstance(cwd, Path)
     
