0ab295df154752b0e7ea7ded221afee44591d5e9
gregnordin <nordin@byu.edu>
2021-09-16 21:19:51 -0600

    Blender: add camera with python

--- Blender/210916_uniform_layers_with_materials.py
+++ Blender/210916_uniform_layers_with_materials.py
@@ -48,12 +48,26 @@ color_RGB = (1, 0.7, 0.2)  # golden
 start_color_RGBA = (*color_RGB, 0)  # transparent
 final_color_RGBA = (*color_RGB, 1)  # opaque
 
+# Single layer
 layer = make_layer(
     "Test_layer", xy_layer_size, xy_layer_size, z_layer_size, z_position=0.0
 )
 
+# Light
 light_data = bpy.data.lights.new("Light", type="POINT")
 light = bpy.data.objects.new("Light", light_data)
 bpy.context.collection.objects.link(light)
 light.location = (4.0762, 1.0055, 5.9039)
 light.rotation_euler = [pi * 37.3 / 180, pi * 3.16 / 180, pi * 107 / 180]
+
+# Camera
+# we first create the camera object
+cam_data = bpy.data.cameras.new("camera")
+cam = bpy.data.objects.new("camera", cam_data)
+bpy.context.collection.objects.link(cam)
+# add camera to scene
+scene = bpy.context.scene
+scene.camera = cam
+# position and rotate camera
+cam.location = (16.344, -15.382, 11.012)
+cam.rotation_euler = [pi * 63.9 / 180, pi * 0.0 / 180, pi * 46.7 / 180]
